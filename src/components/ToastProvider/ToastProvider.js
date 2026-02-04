0c6d0d4dccb4359e4e7065572fd85b747969924b
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2026-02-04 23:43:00 +0000

    Clear toasts with "Esc"

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -5,10 +5,26 @@ export const ToastContext = React.createContext();
 function ToastProvider({ children }) {
   const [toasts, setToasts] = React.useState([]);
 
+  React.useEffect(() => {
+    document.addEventListener("keydown", handleKeyPress)
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress)
+    }
+  })
+
   return (
     <ToastContext.Provider value={{ toasts, setToasts, addToast, removeToast }}>
       {children}
     </ToastContext.Provider>);
+
+  function handleKeyPress({ key }) {
+    if (key === "Escape") clearToasts()
+  }
+
+  function clearToasts() {
+    setToasts([]);
+  }
 
   function addToast({ message, variant }) {
     const newToasts = [...toasts];
