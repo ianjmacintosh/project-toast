d1373e96956888b86eaebc2eb4c0883603eae882
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2026-02-04 04:03:57 +0000

    Add radio-handling component

---
+++ src/components/Radios/Radios.js
@@ -0,0 +1,25 @@
+import React from 'react';
+const RadiosContext = React.createContext();
+
+export function Radios({ children, id, name, value, onChange: callback }) {
+  const data = { name, callback, value };
+
+  return (<RadiosContext.Provider value={data}>{children}</RadiosContext.Provider>);
+}
+
+export function RadioOption({ group, value, children }) {
+  const id = React.useId();
+  const { name, callback, value: controlledValue } = React.useContext(RadiosContext);
+
+  return (<label htmlFor={id} key={value}>
+    <input
+      type="radio"
+      value={value}
+      name={name}
+      id={id}
+      onChange={callback}
+      checked={controlledValue === value}
+    />
+    {children}
+  </label>)
+}
