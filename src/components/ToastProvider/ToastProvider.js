3d3ee0b6d44a12ce5f18b5c15c74cf27e5c0a547
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2026-02-04 23:23:08 +0000

    Add new provider component

---
+++ src/components/ToastProvider/ToastProvider.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export const ToastContext = React.createContext();
+
+function ToastProvider({ children }) {
+  const [toasts, setToasts] = React.useState([]);
+
+  return (
+    <ToastContext.Provider value={{ toasts, setToasts, addToast, removeToast }}>
+      {children}
+    </ToastContext.Provider>);
+
+  function addToast({ message, variant }) {
+    const newToasts = [...toasts];
+
+    newToasts.push({
+      id: crypto.randomUUID(),
+      message,
+      variant
+    })
+
+    setToasts(newToasts)
+  }
+
+  function removeToast(id) {
+    let newToasts = [...toasts].filter((toast) => toast.id !== id);
+
+    setToasts(newToasts)
+  }
+}
+
+export default ToastProvider;
