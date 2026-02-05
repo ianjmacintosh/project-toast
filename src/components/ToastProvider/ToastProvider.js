647dbcb8cb685d9eecfd78b818c92b8dde5e0a78
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2026-02-05 00:05:52 +0000

    Abstract out a key handler hook

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -1,26 +1,17 @@
 import React from 'react';
+import useKeyBinding from '../../hooks/useKeyBinding';
 
 export const ToastContext = React.createContext();
 
 function ToastProvider({ children }) {
   const [toasts, setToasts] = React.useState([]);
 
-  React.useEffect(() => {
-    document.addEventListener("keydown", handleKeyPress)
-
-    return () => {
-      document.removeEventListener("keydown", handleKeyPress)
-    }
-  })
+  useKeyBinding("Escape", () => { clearToasts() })
 
   return (
     <ToastContext.Provider value={{ toasts, setToasts, addToast, removeToast }}>
       {children}
     </ToastContext.Provider>);
-
-  function handleKeyPress({ key }) {
-    if (key === "Escape") clearToasts()
-  }
 
   function clearToasts() {
     setToasts([]);
